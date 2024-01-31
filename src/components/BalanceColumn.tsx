7f2ab7d8c4b891aa9140395ba348d2428dbd8620
EliRE <elisabetre.main@gmail.com>
2024-01-31 17:53:53 +0100

    Fix Card styles. Add conditional class

--- src/components/BalanceColumn.tsx
+++ src/components/BalanceColumn.tsx
@@ -6,7 +6,7 @@ const BalanceColumn = () => {
     <>
       <div className="balance_column_holder">
         <Card name="Shadowheart" balance={123} />
-        <Card name="Lae'zel" balance={123} />
+        <Card name="Lae'zel" balance={-123} />
         <Card name="Karlach" balance={123} />
       </div>
     </>
--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,11 +1,13 @@
 import './Card.css'
 
 const Card = ({ name, balance }: {name: string, balance:number}) => {
+  const card_class = balance > 0 ? "positive_bg" : "negative_bg";
+
   return (
     <>
-      <div className="card">
+      <div className={`card ${card_class}`}>
         <p className="name">{name}</p>
-        <p className="balance">{balance}</p>
+        <p className="balance">{balance} €</p>
       </div>
     </>
   );
