8e73f4464ca9c88a028b55a5112c1c26c582470f
EliRE <elisabetre.main@gmail.com>
2024-02-02 20:34:39 +0100

    Fix tests

---
+++ src/tests/baseFunctions.test.tsx
@@ -0,0 +1,40 @@
+import { it, expect } from 'vitest'
+import {
+  getPersonalDebt,
+  getTotalCost,
+  getPersonalPayments,
+} from "../baseFunctions";
+
+  const inputCosts = [
+    {
+      id: 12358412,
+      person: "person_1",
+      concept: "concept_1",
+      date: new Date(),
+      amount: 250,
+    },
+    {
+      id: 12358413,
+      person: "Minthara",
+      concept: "concept_1",
+      date: new Date(),
+      amount: 50,
+    },
+  ];
+
+  const friend = {id: 1234569, name:'Minthara'}
+
+it("getTotalCost() -> Should add all the costs from a given array of Cost (object)", () => {
+
+  expect(getTotalCost(inputCosts)).toBe(300);
+});
+
+it("getPersonalPayments() -> Should filter the payments by a given friend and get the total amount of them", () => {
+  expect(getPersonalPayments(inputCosts, friend)).toBe(50)
+ });
+
+it("getPersonalDebt() -> Should divide the totalCost between all the friends and subtract the personal payments", () => {
+
+  const friends = 2
+  expect(getPersonalDebt(inputCosts, friends, friend)).toBe(100)
+ })
