12fcc29133e6acd8d73ca569978bef7ac70dd2a4
EliRE <elisabetre.main@gmail.com>
2024-02-02 21:34:58 +0100

    Add rounded

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,13 +1,13 @@
 import './Card.css'
 
 const Card = ({ name, balance }: {name: string, balance:number}) => {
-  const card_class = balance <= 0 ? "positive_bg" : "negative_bg";
+  const card_class = balance >= 0 ? "positive_bg" : "negative_bg";
 
   return (
     <>
       <div className={`card ${card_class}`}>
         <p className="name">{name}</p>
-        <p className="balance">{balance} €</p>
+        <p className="balance">{balance.toFixed(2)} €</p>
       </div>
     </>
   );
