9eb2b0526d851220d5456bb361fade5ba277e02e
EliRE <elisabetre.main@gmail.com>
2024-02-03 15:39:53 +0100

    Fix footer styles

--- src/components/UI/summaryCard.tsx
+++ src/components/UI/summaryCard.tsx
@@ -19,7 +19,10 @@ debt: number})  => {
               <i className="fa-solid fa-arrow-right"></i>
             </span>
             <p className="h1">{creditorName}</p>
-            <p>{debt}<span>€</span></p>
+            <p>
+              {debt}
+              <span>&nbsp;€</span>
+            </p>
           </div>
         </div>
       </div>
